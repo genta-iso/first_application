14741711e1258984c4f8f43b10ab9a741508fb6f
genta-iso <i.am.genta@icloud.com>
2024-06-10 01:23:18 +0900

    最初のコミット

---
+++ bingo.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import { reactive } from 'vue'
+
+const counter = reactive({
+  count: 0
+})
+
+console.log(counter.count) // 0
+counter.count++
+
+const message = ref('Hello World!')
+console.log(message.value) // "Hello World!"
+message.value = 'Changed'
+// component logic
+// declare some reactive state here.
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <h1>{{ message.split('').reverse().join('') }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
